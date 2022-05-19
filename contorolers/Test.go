df4b2ae4d985ff64112e47240997439d83484bc5
MrM <MMMohebi@outlook.com>
2022-05-19 12:22:39 +0430

    remove test

--- contorolers/Test.go
+++
@@ -1,9 +0,0 @@
-package contorolers
-
-import "github.com/gin-gonic/gin"
-
-func Test(context *gin.Context) {
-	context.JSON(200, gin.H{
-		"asdasd": "asdasd",
-	})
-}
--- router/Router.go
+++ router/Router.go
@@ -7,10 +7,7 @@ import (
 
 func Routs(r *gin.Engine) {
 	AssetsRoute(r)
-
 	r.LoadHTMLGlob("templates/**/*.html")
-
-	r.GET("/test", contorolers.Test)
 
 	r.GET("/", contorolers.Index)
 	r.GET("/docs", contorolers.Docs)
