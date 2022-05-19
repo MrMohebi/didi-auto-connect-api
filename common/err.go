10f9fd96d7a9adf57dabcc4a7d0f59956714aa09
MrM <MMMohebi@outlook.com>
2022-05-19 10:56:18 +0430

    common.err

---
+++ common/err.go
@@ -0,0 +1,13 @@
+package common
+
+import "log"
+
+func IsErr(err error, message ...string) {
+	if err != nil {
+		if len(message) == 0 {
+			log.Fatal(message)
+			return
+		}
+		log.Fatal(err)
+	}
+}
