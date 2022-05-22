2429478864b47145e071c3aaabcf39c4cf1b3a71
MrM <MMMohebi@outlook.com>
2022-05-22 16:57:50 +0430

    login init

--- contorolers/login.go
+++ contorolers/login.go
@@ -1,17 +1,20 @@
 package contorolers
 
 import (
+	"context"
 	"github.com/MrMohebi/didi-auto-connect-api.git/common"
 	"github.com/MrMohebi/didi-auto-connect-api.git/faces"
 	"github.com/MrMohebi/didi-auto-connect-api.git/models"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"net/http"
+	"time"
 )
 
 func Login() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		//ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		//defer cancel()
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
 
 		var reqBody faces.LoginReq
 		common.ValidBindForm(c, &reqBody)
@@ -19,17 +22,26 @@ func Login() gin.HandlerFunc {
 		var user models.User
 
 		isLimit := false
-		token := common.RandStr(16)
+		token := common.RandStr(32)
 
-		//err := models.UsersCollection.FindOne(ctx, bson.M{"$regex": reqBody.Username, "$options": "i"}).Decode(&user)
-		//common.IsErr(err)
+		isJoined := false
+		err := models.UsersCollection.FindOne(ctx, bson.M{"username": bson.M{"$regex": reqBody.Username, "$options": "i"}}).Decode(&user)
+		if err != nil {
+			isJoined = true
+		}
+
+		if !isJoined {
+			singUp()
+			return
+		}
+
+		login()
 
 		c.JSON(http.StatusOK, gin.H{
-			"asdasd":  isLimit,
-			"aaaaa":   token,
-			"usename": user.Username,
+			"token":     token,
+			"hasAccess": !isLimit,
+			"isLimit":   isLimit,
 		})
-
 	}
 }
 
